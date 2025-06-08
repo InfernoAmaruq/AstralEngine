710bebed4a9fbd6b121ada0cb6007b7355bd11b5
bjorn <mail@bjornbyt.es>
2025-06-08 14:55:03 -0700

    Disable graphics tests;

--- test/conf.lua
+++ test/conf.lua
@@ -1,6 +1,6 @@
 function lovr.conf(t)
   t.identity = 'test'
-  t.modules.graphics = not os.getenv or not os.getenv('CI')
+  t.modules.graphics = os.getenv and not os.getenv('CI')
   t.window = nil
 end
 

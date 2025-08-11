59fc7a1f11420d95cad89d29b3eefd51c88caaa0
bjorn <mail@bjornbyt.es>
2025-08-10 18:58:03 -0700

    Debugging stuck tests;

--- test/lovr/init.lua
+++ test/lovr/init.lua
@@ -1,7 +1,10 @@
+io.stdout:setvbuf('no')
+print('running tests')
 group('lovr', function()
   for i, file in ipairs(lovr.filesystem.getDirectoryItems('lovr')) do
     local module = file:match('%a+')
     if lovr[module] then
+      print(('running %s tests'):format(module))
       require('lovr/' .. module)
     end
   end

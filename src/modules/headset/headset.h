bf19782121b295f0cb78c7599b31dc9b62f7f154
bjorn <mail@bjornbyt.es>
2025-11-20 10:43:40 -0800

    Fix windows build;

--- src/modules/headset/headset.h
+++ src/modules/headset/headset.h
@@ -252,6 +252,3 @@ bool lovrHeadsetIsSupported(void);
 void lovrHeadsetGetVulkanPhysicalDevice(void* instance, uintptr_t physicalDevice);
 uint32_t lovrHeadsetCreateVulkanInstance(void* instanceCreateInfo, void* allocator, uintptr_t instance, void* getInstanceProcAddr);
 uint32_t lovrHeadsetCreateVulkanDevice(void* instance, void* deviceCreateInfo, void* allocatoor, uintptr_t device, void* getInstanceProcAddr);
-uintptr_t xr_get_instance(void);
-uintptr_t xr_get_system(void);
-uintptr_t xr_get_session(void);

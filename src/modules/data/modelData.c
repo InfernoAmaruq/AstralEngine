6c6a06d73b4b59399574b63f4dd315342f672c6c
bjorn <mail@bjornbyt.es>
2026-04-09 11:18:11 -0700

    rm unused function;

--- src/modules/data/modelData.c
+++ src/modules/data/modelData.c
@@ -43,16 +43,6 @@ void lovrModelDataDestroy(void* ref) {
   lovrFree(model);
 }
 
-static uint32_t nextPo2(uint32_t x) {
-  x--;
-  x |= x >> 1;
-  x |= x >> 2;
-  x |= x >> 4;
-  x |= x >> 8;
-  x |= x >> 16;
-  return x + 1;
-}
-
 // Batches allocations for all the ModelData arrays
 void lovrModelDataAllocate(ModelData* model) {
   ModelMetadata* meta = &model->meta;

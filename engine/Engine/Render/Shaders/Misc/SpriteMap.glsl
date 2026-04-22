f3631792f5993b2063f489470998438cc9fd72c7
InfernoAmaruq <flameytrec@gmail.com>
2026-04-22 16:34:36 +0100

    func

--- engine/Engine/Render/Shaders/Misc/SpriteMap.glsl
+++ engine/Engine/Render/Shaders/Misc/SpriteMap.glsl
@@ -6,6 +6,10 @@ uniform AtlasData {
     ivec4 Atlas;
 };
 
+ivec2 TEXTURE_ATLAS_GET_PIXEL(vec2 UV){
+    return ivec2(floor(Atlas.xy - 1)) + ivec2(floor(UV * (Atlas.zw - 1)));
+}
+
 vec4 astral_main(){
     if (Atlas.x >= 1){
         ivec2 Pixel = ivec2(floor(Atlas.xy - 1)) + ivec2(floor(UV * (Atlas.zw - 1)));
